e25329720d01c1e9dee6b5d447c8f69d42f61bcb
Madhusmita47 <madhusmita238091@gmail.com>
2023-04-15 20:15:04 +0530

    Final Push

---
+++ src/App.js
@@ -0,0 +1,25 @@
+
+import Header from './Components/Header';
+import Signup from './Components/Signup';
+import Login from './Components/Login'
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
+
+function App() {
+  const heading = "Mern"
+
+  return (
+    <Router>
+      <Header title={heading} />
+     
+      <Routes>
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/' element={<Login />} />
+      </Routes>
+     
+     
+    </Router>
+  );
+}
+
+export default App;
